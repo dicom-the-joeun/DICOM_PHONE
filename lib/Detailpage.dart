d80933dfc6aa1684a7d33fa1bd553aaa418afebd
김종진 <rlawhdwls55@naver.com>
2024-01-03 19:04:13 +0900

    아이콘 클릭 시 색상 넣기 완료

--- lib/Detailpage.dart
+++ lib/Detailpage.dart
@@ -8,49 +8,54 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('PACSPLUS3'),
-         iconTheme: IconThemeData(
-          color: Colors.red
-        ),
+        iconTheme: IconThemeData(color: Colors.red),
       ),
-       bottomNavigationBar: BottomNavigationBar(
-         type: BottomNavigationBarType.fixed,
-    items:const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_display_rounded),
-            label: '윈도우라벨',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.flip),
-            label: '흑백반전',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.swipe_sharp),
-            label: '스크롤 루프',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.slow_motion_video),
-            label: '플레이 클립',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.zoom_in),
-            label: '확대',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.zoom_out),
-            label: '축소',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.flip),
-            label: '수평반전',
-          ),
-        ],
-      selectedItemColor: Colors.red
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_display_rounded),
+              label: '윈도우라벨',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.flip),
+              label: '흑백반전',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.swipe_sharp),
+              label: '스크롤 루프',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.slow_motion_video),
+              label: '플레이 클립',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.zoom_in),
+              label: '확대',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.zoom_out),
+              label: '축소',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.flip),
+              label: '수평반전',
+            ),
+          ],
+          selectedItemColor: Colors.red),
     );
   }
 }
