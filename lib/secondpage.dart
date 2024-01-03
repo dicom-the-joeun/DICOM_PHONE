90232ded3dcb32a0ff92658a305db72f91b65896
김종진 <rlawhdwls55@naver.com>
2024-01-03 18:59:25 +0900

    디테일페이지 버튼,화면 구현

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -1,4 +1,6 @@
+import 'package:dicom_phone/thirdpage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SecondPage extends StatefulWidget {
   const SecondPage({super.key});
@@ -10,6 +12,25 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PACSPLUS2'),
+        iconTheme: IconThemeData(
+          color: Colors.red
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [           
+            ElevatedButton(
+              onPressed: (){
+                Get.to(DetailPage());
+              },
+              child: Text('상세페이지 이동'))
+          ],
+        ),
+      ),
+    );
+    
   }
 }
