240dfc4fd2fba754bdbf0964f1b3a739660bfb2b
김종진 <rlawhdwls55@naver.com>
2024-01-15 16:54:42 +0900

    version 변경,디테일 페이지 다중 선택,기능 가능(비디오클립,화면밝기 제외) 현재 선택된 탭 색 표시 진행 중

--- lib/Detailpage.dart
+++ lib/Detailpage.dart
@@ -10,20 +10,19 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-  int _currentIndex = 0;
+  late List<int> currentIndex;
   late List<String> fruitList;
-  bool allowZoom = false;
-  bool bllowZoom = false;
-  bool cllowZoom = false;
-  bool dllowZoom = false;
+  Set<int> selectedTabs = {}; // Set을 사용하여 다중 선택을 관리합니다.
   double brightness = 0;
-  bool isImageColored = false;
-  bool isImageColor = false;
   
+
+  double sliderValue = 0.0;
+  int currentImageIndex = 0;
 
   @override
   void initState() {
     super.initState();
+    currentIndex = [0, 1, 2, 3, 4, 5];
     fruitList = [
       'Apple.png',
       'Banana.png',
@@ -32,20 +31,9 @@ class _DetailPageState extends State<DetailPage> {
       'Pineapple.png',
       'Watermelon.png',
     ];
+
   }
 
-  void _toggleImageColor() {
-  setState(() {
-    isImageColored = !isImageColored;
-  });
-}
-
-void toggleImageColor() {
-  setState(() {
-    isImageColored  = !isImageColored ;
-  });
-}
-  
 
 
   @override
@@ -62,133 +50,141 @@ void toggleImageColor() {
               itemCount: fruitList.length,
               itemBuilder: (context, index) {
                 return SizedBox(
-                  height: 230, // 이미지 크기
+                  height: 230,
                   child: Stack(
                     children: [
-                      _findex(index),
+                      _buildImage(index),
                     ],
                   ),
                 );
               },
             ),
           ),
-          if (_currentIndex == 0) // 0번째 탭에서만 슬라이더 표시
-            Positioned(
-              top: 150, // slider 오른쪽 화면 중앙에 배치
-              right: 0, // slider 오른쪽 화면에 배치
-              child: RotatedBox(
-                quarterTurns: 1,
-                child: Slider(
-                  value: brightness, 
-                  min: 0.0,
-                  max: 1.0,
-                  onChanged: (value) {
-                    setState(() {
-                      brightness = value;
-                    });
-                  },
-                ),
-              ),
+         Positioned(
+            top: 150,
+            right: 0,
+            child: RotatedBox(
+              quarterTurns: 1,
+                   child: selectedTabs.contains(0) // 1번 탭에서만 슬라이더 표시
+        ? Slider(
+            value: brightness,
+            min: 0.0,
+            max: 1.0,
+            onChanged: (value) {
+              setState(() {
+                brightness = value;
+              });
+            },
+          )
+        : selectedTabs.contains(2) // 2번 탭에서만 슬라이더 표시
+            ? Slider(
+                value: sliderValue,
+                min: 0.0,
+                max: 1.0,
+                onChanged: (value) {
+                  setState(() {
+                    changeImage(value);
+                  });
+                },
+              )
+            : Container(),
             ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-         currentIndex: _currentIndex,
-  onTap: (index) {
-      if (_currentIndex == index) {
-          toggleImageColor();
-      } 
-    setState(() {
-        _currentIndex = index;
-    });
-  
-        
+        currentIndex: selectedTabs.isNotEmpty ? selectedTabs.first : 0,
+        onTap: (index) {
+
+          setState(() {
+
+            if (selectedTabs.contains(index)) {
+              selectedTabs.remove(index);
+            } else {
+              selectedTabs.add(index);
+            }
+          });
         },
         type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_display_rounded),
-            label: '윈도우레벨',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.flip),
-            label: '흑백반전',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.swipe_sharp),
-            label: '스크롤 루프',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.slow_motion_video),
-            label: '플레이 클립',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.zoom_in),
-            label: '확대/축소',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.flip),
-            label: '수평반전',
-          ),
-        ],
-        selectedItemColor: Colors.red,
-      ),
+        
+       items: const <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      icon: Icon(Icons.settings_display_rounded),
+      label: '윈도우레벨',
+     
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.flip),
+      label: '흑백반전',
+      
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.swipe_sharp),
+      label: '스크롤 루프',
+    
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.slow_motion_video),
+      label: '플레이 클립',
+      
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.zoom_in),
+      label: '확대/축소',
+      
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.flip),
+      label: '수평반전',
+     
+    ),
+  ],
+),
+      backgroundColor: Colors.red,
     );
   }
 
-  Widget _findex(int index) {
-  dllowZoom = _currentIndex == 1; // 흑백반전 탭
-  allowZoom = _currentIndex == 4; // 확대/축소 탭
-  bllowZoom = _currentIndex == 5; // 화면전환 탭
+  Widget _buildImage(int index) {
+    bool allowZoom = selectedTabs.contains(4); // 확대/축소 탭
+    bool bllowZoom = selectedTabs.contains(5); // 화면전환 탭
 
-     return Stack(
-      children: [
-        ColorFiltered(
-          colorFilter: ColorFilter.mode(
-            Colors.white.withOpacity(1 - brightness),
-            BlendMode.modulate,
-          ),
-          child: Transform(
-            alignment: Alignment.center,
-            transform: bllowZoom
-                ? Matrix4.rotationY(math.pi) // 5번째 탭일 때 좌우반전
-                : Matrix4.identity(),
-            child: PhotoView(
-              
-              imageProvider: AssetImage('images/${fruitList[index]}'),
-              minScale: allowZoom
-                  ? PhotoViewComputedScale.contained * 0.8
-                  : PhotoViewComputedScale.contained,
-              maxScale: allowZoom
-                  ? PhotoViewComputedScale.covered * 1.8
-                  : PhotoViewComputedScale.contained,
-                  
+    return ColorFiltered(
+      colorFilter: selectedTabs.contains(1)
+          ? const ColorFilter.mode(
+              Colors.grey,
+              BlendMode.color,
+            )
+          : const ColorFilter.mode(
+              Colors.transparent,
+              BlendMode.saturation,
             ),
+        child: Transform(
+          alignment: Alignment.center,
+          transform: bllowZoom
+              ? Matrix4.rotationY(math.pi) // 5번째 탭일 때 좌우반전
+              : Matrix4.identity(),
+          child: PhotoView(
+            imageProvider: AssetImage('images/${fruitList[currentImageIndex]}'),
+            minScale: allowZoom
+                ? PhotoViewComputedScale.contained * 0.8
+                : PhotoViewComputedScale.contained,
+            maxScale: allowZoom
+                ? PhotoViewComputedScale.covered * 1.8
+                : PhotoViewComputedScale.contained,
           ),
         ),
-       
-   if (_currentIndex == 1)
-          Positioned.fill(
-            child: IgnorePointer(
-              ignoring: true,
-              child: ColorFiltered(
-                colorFilter: isImageColored
-                    ? const ColorFilter.mode(
-                        Colors.transparent,
-                        BlendMode.saturation,
-                      )
-                    : const ColorFilter.mode(
-                        Colors.grey,
-                        BlendMode.saturation,
-                      ),
-                child: Image.asset(
-                  'images/${fruitList[index]}',
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-            ),
-          ),
-      ],
+      
     );
+  }
+
+  void changeImage(double value) {
+    // Calculate the index based on the slider value
+    int index = (value * (fruitList.length - 1)).round();
+
+    // Update the currentImageIndex
+    setState(() {
+      currentImageIndex = index;
+      sliderValue = value;
+    });
   }
 }
